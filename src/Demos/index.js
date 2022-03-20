5a254f26aa63d8e987d94c8188b3e188245dd53d
Th3M4ttman <Workmatthewjharris94@gmail.com>
2022-03-20 19:56:51 +0000

    MicroDemo

--- src/Demos/index.js
+++ src/Demos/index.js
@@ -184,7 +184,7 @@ export class Micro_Demo extends Demo{
 }
 
 const micro_demos = {
-  test, Test: new Micro_Demo((e) => {return "Poop"}, "Test", "Text here","https://google.com", ["Javascript"])
+  Test: new Micro_Demo((e) => {return "Poop"}, "Test", "Text here","https://google.com", ["Javascript"])
 };
 
 export class MicroDemoPage extends Component{
