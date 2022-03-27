13fcc094d13449766e223989c5573cb6cd6acfee
Th3M4ttman <Workmatthewjharris94@gmail.com>
2022-03-27 03:32:52 +0100

    Certificates

--- src/Components/Certificates.js
+++ src/Components/Certificates.js
@@ -75,7 +75,7 @@ class Certificate extends Component{
         </div>
         <br/>
         <img className="certimg" src={cert.imgurl} alt={cert.title} onClick={() => this.setState({ isOpen: true })} />
-        <br/><br/><p className="enlarge">click to enlarge</p>
+        <br/><br/><p className="enlarge"onClick={() => this.setState({ isOpen: true })}>click to enlarge</p>
       </div>
       <br/>
       {isOpen && (
@@ -89,6 +89,14 @@ class Certificate extends Component{
 }
 
 const certificates = [
+  new certificate("Foreign Languages", "Japanese", "GCSE", "/Certificate/Math.jpg"),
+  new certificate("Science", "Science", "GCSE", "/Certificate/Science.jpg"),
+  new certificate("English", "Adult Literacy", "GCSE", "/Certificate/English.jpg"),
+  new certificate("Math", "Math", "GCSE", "/Certificate/Math.jpg"),
+  new certificate("IT", "Extended Btec in Videogame and Software Development", "BTEC", "/Certificate/Extended-Btec-IT.jpg"),
+  new certificate("IT", "Btec in Videogame and Software Development", "BTEC", "/Certificate/Btec-IT.jpg"),
+  new certificate("IT", "Award in Digital Applications", "GCSE", "/Certificate/AIDA.jpg"),
+  new certificate("IT", "Functional IT", "GCSE", "/Certificate/Func-English.jpg"),
   new certificate("IT", "C++", "Online Course", "/Certificate/Cpp.jpg"),
   new certificate("IT", "Javascript", "Online Course", "/Certificate/JavaScript.jpg"),
   new certificate("IT", "Machine Learning", "Online Course", "/Certificate/MachineLearning.jpg"),
@@ -100,10 +108,105 @@ const certificates = [
 ]
 
 
-const certificate_comps = certificates.map(
-  (cert) => {
-    return (<Certificate cert={cert} />);
+const certificate_comps = (subject, level) => {
+  return certificates.map((cert) => {
+    if (cert.subject === subject && cert.level === level){
+      return (<Certificate cert={cert} />);
+    }
+    return "";
   });
+}
+
+class Subject extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      subject: props.subject,
+      open: false
+    }
+    this._online = certificate_comps(this.state.subject, "Online Course")
+    this._gcse = certificate_comps(this.state.subject, "GCSE")
+    this._btec = certificate_comps(this.state.subject, "BTEC")
+  }
+  online(){
+    let x = 0;
+    for (let cert of certificates){
+      if (cert.subject === this.state.subject && cert.level === "Online Course"){
+        x += 1;
+      }
+    }
+    if (x > 0){
+      return(
+        <>
+        {title("Online Courses", true, false)}<br/>
+        {this._online}
+        </>
+      );}
+    return ""
+  }
+  gcse(){
+    let x = 0;
+    for (let cert of certificates){
+      if (cert.subject === this.state.subject && cert.level === "GCSE"){
+        x += 1;
+      }
+    }
+    if (x > 0){
+      return(
+        <>
+        {title("Level 2 (GCSE)", true, false)}<br/>
+        {this._gcse}
+        </>
+      );}
+    return ""
+  }
+  btec(){
+    let x = 0;
+    for (let cert of certificates){
+      if (cert.subject === this.state.subject && cert.level === "BTEC"){
+        x += 1;
+      }
+    }
+    if (x > 0){
+      return(
+        <>
+        {title("Level 3 (NVQ)", true, false)}<br/>
+        {this._btec}
+        </>
+      );}
+    return ""
+  }
+  show(){
+    if (this.state.open) {
+      return (
+        <>
+        <br/><br/>
+        {this.btec()}
+        {this.gcse()}
+        {this.online()}
+        
+        </>
+      );
+    } else {
+      return ""
+    }
+  }
+  buttonclass(){
+    if (this.state.open === true){
+      return "certbuttont"
+    }
+    return "certbuttonf"
+  }
+  render(){
+    return (
+      <>
+      <button className={this.buttonclass()} onClick={(e) => {this.setState({open: !this.state.open})}}>{this.state.subject}</button>
+      {this.show()}
+      <br/><br/>
+      </>
+    );
+  }
+}
 
 export default class Certificates extends Component{
   render(){
@@ -114,7 +217,13 @@ export default class Certificates extends Component{
         {title("", true, true)}
         <br/>
         <div className="innercerts">
-        {certificate_comps}
+          <p className="enlarge">click subjects to expand</p>
+          <Subject subject="IT" />
+          <Subject subject="Math" />
+          <Subject subject="English" />
+          <Subject subject="Science" />
+          <Subject subject="Foreign Languages" />
+          <Subject subject="Misc" />
         </div>
       </div>
       </div>
